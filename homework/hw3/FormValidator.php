003cf670618caaa36e73b1a04a4d89f7c890addf
Pavel Sarlov <sarlovpavel@gmail.com>
2022-04-27 19:10:16 +0300

    update

--- homework/hw3/FormValidator.php
+++ homework/hw3/FormValidator.php
@@ -16,12 +16,12 @@ class FormValidator {
     public function validateName($data) {
         $prop = "name";
 
-        if (!property_exists($data, $prop)) {
+        if (!isset($data[$prop])) {
             $this->result->addError($prop, "Името на курса е задължително поле");
             return;
         }
 
-        $len = mb_strlen($data->{$prop}, "UTF-8");
+        $len = mb_strlen($data[$prop], "UTF-8");
         $min = 2;
         $max = 150;
         if ($len < $min || $len > $max) {
@@ -32,12 +32,12 @@ class FormValidator {
     public function validateTeacher($data) {
         $prop = "teacher";
 
-        if (!property_exists($data, $prop)) {
+        if (!isset($data[$prop])) {
             $this->result->addError($prop, "Името на преподавателя е задължително поле");
             return;
         }
 
-        $len = mb_strlen($data->{$prop}, "UTF-8");
+        $len = mb_strlen($data[$prop], "UTF-8");
         $min = 3;
         $max = 200;
         if ($len < $min || $len > $max) {
@@ -48,29 +48,29 @@ class FormValidator {
     public function validateDescription($data) {
         $prop = "description";
 
-        if (!property_exists($data, $prop)) {
+        if (!isset($data[$prop])) {
             $this->result->addError($prop, "Описанието на курса е задължително поле");
             return;
         }
 
-        $len = mb_strlen($data->{$prop}, "UTF-8");
+        $len = mb_strlen($data[$prop], "UTF-8");
         $min = 10;
         if ($len < $min) {
-            $this->result->addError($prop, "Описанието на курса трябва да е поне {$min}, а вие сте въвели {$len}");
+            $this->result->addError($prop, "Описанието на курса трябва да е поне {$min} символа, а вие сте въвели {$len}");
         }
     }
 
     public function validateGroup($data) {
         $prop = "group";
 
-        if (!property_exists($data, $prop)) {
+        if (!isset($data[$prop])) {
             $this->result->addError($prop, "Групата е задължително поле");
             return;
         }
 
         $options = ["М", "ПМ", "ЯКН", "ОКН"];
         $_ = implode(", ", $options);
-        if (!in_array($data->{$prop}, $options)) {
+        if (!in_array($data[$prop], $options)) {
             $this->result->addError($prop, "Невалидна група, изберете една от {$_}");
         }
     }
@@ -78,12 +78,12 @@ class FormValidator {
     public function validateCredits($data) {
         $prop = "credits";
 
-        if (!property_exists($data, $prop)) {
+        if (!isset($data[$prop])) {
             $this->result->addError($prop, "Кредитите са задължително поле");
             return;
         }
 
-        $cred = $data->{$prop};
+        $cred = $data[$prop];
         if (filter_var($cred, FILTER_VALIDATE_INT) === false || $cred <= 0) {
             $this->result->addError($prop, "Кредитите трябва да са цяло положително число, а вие сте въвели {$cred}");
         }
--- homework/hw3/index.php
+++ homework/hw3/index.php
@@ -2,8 +2,13 @@
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require "FormValidator.php";
-
-    $data = json_decode(file_get_contents("php://input"));
+    
+    if (array_key_exists('Content-Type', getallheaders())) {
+        $data = json_decode(file_get_contents("php://input"), true);
+    }
+    else {
+        $data = $_POST;
+    }
 
     $validator = new FormValidator();
 
