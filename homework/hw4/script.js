793ff52d9699aba2d2374fe4a502429bc02a4a4a
Pavel Sarlov <sarlovpavel@gmail.com>
2022-05-23 01:24:23 +0300

    update hw4

--- homework/hw4/script.js
+++ homework/hw4/script.js
@@ -3,7 +3,7 @@ window.onload = async () => {
         .then(resp => resp.json());
 
     function removeAlerts() {
-        document.querySelectorAll(".error, .success").forEach(e => e.parentNode.removeChild(e));
+        document.querySelectorAll(".alert").forEach(e => e.parentNode.removeChild(e));
     }
 
     function popAlert(success, msg, parent) {
@@ -11,6 +11,13 @@ window.onload = async () => {
         alert.textContent = msg;
         alert.classList.add("alert");
         alert.classList.add((success ? "success" : "error"));
+
+        if (success) {
+            let a = document.createElement("a");
+            a.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            a.innerHTML = "Вход";
+            alert.appendChild(a);
+        }
 
         parent.append(alert);
     }
@@ -32,22 +39,22 @@ window.onload = async () => {
         let postalCode = document.getElementById("postal-code");
 
         if (!username.value.match(/^\w{3,10}$/)) {
-            errors.push(['Невалидно потребителско име', username.parentNode]);
+            errors.push(['Невалидно потребителско име. Трябва да съдържа между 3 и 10 символа включително.', username.parentNode]);
         }
         if (!password.value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\da-zA-Z]{6,10}$/)) {
-            errors.push(['Невалидно парола', password.parentNode]);
+            errors.push(['Невалидна парола. Трябва да съдържа между 6 и 10 символа включително, малка буква, главна буква и цифра.', password.parentNode]);
         }
         if (!email.value.match(/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/)) {
-            errors.push(['Невалидно имейл', email.parentNode]);
+            errors.push(['Невалиден имейл. Трябва отговаря на общоприетия формат от RFC 5322.', email.parentNode]);
         }
         if (!firstName.value.match(/^\w{1,50}$/)) {
-            errors.push(['Невалидно име', firstName.parentNode]);
+            errors.push(['Невалидно име. Трябва да съдържа до 50 символа.', firstName.parentNode]);
         }
         if (!familyName.value.match(/^\w{1,50}$/)) {
-            errors.push(['Невалидно фамилия', familyName.parentNode]);
+            errors.push(['Невалидна фамилия. Трябва да съдържа до 50 символа.', familyName.parentNode]);
         }
-        if (!postalCode.value.match(/^\d*(-\d+)?$/)) {
-            errors.push(['Невалидно пощенски код', postalCode.parentNode]);
+        if (!postalCode.value.match(/^((\d{5}-)?\d{4})?$/)) {
+            errors.push(['Невалиден пощенски код. Трябва да е във формат 1111 или 11111-1111 и да съдържа само цифри.', postalCode.parentNode]);
         }
 
         if (errors.length != 0) {
@@ -58,11 +65,11 @@ window.onload = async () => {
         else {
             for (let user of users) {
                 if (user.username == username.value) {
-                    popAlert(false, `Вече съществува потребител с потребителско име ${username.value}`, form);
+                    popAlert(false, `Вече съществува потребител с потребителско име ${username.value}.`, form);
                     return;
                 }
             }
-            popAlert(true, "Регистрацията успешна", form);
+            popAlert(true, "Регистрацията успешна. ", form);
         }
     });
 };
