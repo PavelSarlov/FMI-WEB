ceae1a2b60c2132a6311ca2b95e7f73c32f49703
Pavel Sarlov <sarlovpavel@gmail.com>
2022-06-01 16:16:34 +0300

    update

--- practice/exercises/ex9/frontend/admin/script.js
+++ practice/exercises/ex9/frontend/admin/script.js
@@ -1,78 +1,11 @@
-import * as utils from "../utils.js";
-
 window.onload = () => {
     utils.authorize();
-    utils.mapButtons();
 
-    let hideForm = (form) => {
+    let hidePanel = (panel) => {
         form.style.display = "none";
     }
 
-    let showForm = (form) => {
+    let showPanel = (panel) => {
         form.style.display = "flex";
     }
-
-    document.getElementById("btn-admin-panel").addEventListener("click", () => {
-        utils.redirectTo("../admin/admin-panel.html");
-    });
-
-    (async () => {
-        let productTypes = getProductTypes();
-
-        let opt = document.getElementById("product-type");
-
-        for (let prod of productTypes) {
-            let sel = document.createElement("option");
-            sel.setAttribute("value", prod.id);
-            sel.setAttribute("name", prod.name);
-            sel.innerHTML = prod.name;
-
-            opt.appendChild(sel);
-        }
-    })();
-
-    document.getElementById("btn-add-product").addEventListener("click", (event) => {
-        event.preventDefault();
-
-        let reqInfo = {
-            method: "POST",
-            body: JSON.stringify({
-                productType: document.getElementById("product-type").value,
-                name: document.getElementById("product-name").value
-            }),
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            }
-        };
-
-        fetch("../../backend/api/products.php", reqInfo)
-            .then(resp => resp.json())
-            .then(msg => console.log(msg));
-    });
-    
-    let updateTab = (tab) => {
-        [...document.getElementsByClassName("tab")].forEach(e => e.classList.remove("tab-selected"));
-        tab.classList.add("tab-selected");
-
-        switch(tab.name) {
-            case "delete": {
-                [...document.getElementsByTagName("form")].forEach(fm => hideForm(fm));
-                showForm(document.getElementById("form-delete"));
-                let products = getAllProducts();
-                
-                for (let prod of products) {
-                    let li = document.createElement("li");
-                }
-
-                break;
-            }
-            default: {
-                [...document.getElementsByTagName("form")].forEach(fm => hideForm(fm));
-                showForm(document.getElementById("form-create"));
-            }
-        }
-    }
-
-    updateTab(document.querySelector('.tab-selected'));
-    [...document.getElementsByClassName("tab")].forEach(t => t.addEventListener("click", (event) => updateTab(event.target)));
 };
--- project/db-populate.sql
+++ project/db-populate.sql
@@ -1,5 +1,9 @@
+USE ccs_db;
+
 SET
-  @student = UUID();
+  @student1 = UUID();
+SET
+  @student2 = UUID();
 
 SET
   @teacher = UUID();
@@ -10,24 +14,20 @@ SET
 INSERT INTO
   users
 VALUES
-  (
-    @student,
-    "student",
-    PASSWORD("student"),
-    1,
-    "Software Degradation",
-    "Mahalata",
-    "USER"
-  ),
-  (
-    @teacher,
-    "teacher",
-    PASSWORD("teacher"),
-    null,
-    null,
-    null,
-    "ADMIN"
-  );
+  (@student1, "student1", "student1@fmi.bg", PASSWORD("student1"), "USER"),
+  (@student2, "student2", "student2@fmi.bg", PASSWORD("student2"), "USER"),
+  (@teacher, "teacher", "teacher@fmi.bg", PASSWORD("teacher"), "ADMIN");
+
+INSERT INTO
+  students
+VALUES
+  (@student1, "FACULTY1", 1, "Software Degradation", "Mahalata"),
+  (@student2, "FACULTY2", 1, "Software Degradation", "Mahalata");
+
+INSERT INTO
+  teachers
+VALUES
+  (@teacher);
 
 INSERT INTO
   chat_rooms(id, name)
@@ -37,10 +37,9 @@ VALUES
 INSERT INTO
   user_chats
 VALUES
-  (UUID(), @student, @chat_room, TRUE);
+  (UUID(), @student1, @chat_room, CURRENT_TIMESTAMP, TRUE);
 
 INSERT INTO
   messages(userId, chatRoomId, content)
 VALUES
-  (@student, @chat_room, "Veni Vidi Vici");
-
+  (@student1, @chat_room, "Veni Vidi Vici");
